5184d25ef2de7a2925749718ef2da3f3037f1b44
WaiKinTANG <wk.tang@protonmail.com>
2023-09-23 17:59:14 +0800

    nearBy

--- routes/maze.py
+++ routes/maze.py
@@ -15,28 +15,28 @@ def mazerun():
     input_value = data.get("input")
     result = {"playerAction":0}
 
-    if input_value["nearby"][2][1] == 2:
+    if input_value["nearBy"][2][1] == 2:
         result["playerAction"] = 'down'
 
-    elif input_value["nearby"][0][1] == 2:
+    elif input_value["nearBy"][0][1] == 2:
         result["playerAction"] = 'up'
 
-    elif input_value["nearby"][1][0] == 2:
+    elif input_value["nearBy"][1][0] == 2:
         result["playerAction"] = 'left'
     
-    elif input_value["nearby"][1][2] == 2:
+    elif input_value["nearBy"][1][2] == 2:
         result["playerAction"] = 'right'
 
-    elif (input_value["nearby"][2][1] == 0) and (input_value["nearby"][1][2] == 1):
+    elif (input_value["nearBy"][2][1] == 0) and (input_value["nearby"][1][2] == 1):
         result["playerAction"] = 'right'
 
-    elif (input_value["nearby"][1][0] == 0) and (input_value["nearby"][2][1] == 1):
+    elif (input_value["nearBy"][1][0] == 0) and (input_value["nearby"][2][1] == 1):
         result["playerAction"] = 'down'
 
-    elif (input_value["nearby"][0][1] == 0) and (input_value["nearby"][1][0] == 1):
+    elif (input_value["nearBy"][0][1] == 0) and (input_value["nearby"][1][0] == 1):
         result["playerAction"] = 'left'
 
-    elif (input_value["nearby"][1][2] == 0) and (input_value["nearby"][0][1] == 1):
+    elif (input_value["nearBy"][1][2] == 0) and (input_value["nearby"][0][1] == 1):
         result["playerAction"] = 'up'
     
     
