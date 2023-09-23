d13a4c498025ff4c526ad971213819b947259532
WaiKinTANG <wk.tang@protonmail.com>
2023-09-23 23:09:43 +0800

    partyyyyyyyy

--- routes/chinese.py
+++ routes/chinese.py
@@ -10,6 +10,6 @@ logger = logging.getLogger(__name__)
 
 @app.route('/chinese-wall', methods=['GET'])
 def chinn():
-    result = {"1":"Fluffy","2":"Galactic","3":"Mangoes","4":"Subatomic","5":"Dance"}
+    result = {"1":"Fluffy","2":"Galactic","3":"Mangoes","4":"Subatomic","5":"Party"}
     return json.dumps(result)
 
